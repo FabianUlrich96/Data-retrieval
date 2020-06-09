94e95f230129166d3a5c27ee3137b3d285779177
Fabian Ulrich <fabian.ulrich@stud.uni-bamberg.de>
2020-06-09 19:48:39 +0200

    modifying related_tag_count because of counting issue wip.

--- filter_tags.py
+++ filter_tags.py
@@ -3,6 +3,7 @@ import os.path
 import sys
 import re
 from UserSelection import UserSelection
+from CsvAction import CsvAction
 
 
 def input_filter():
@@ -24,13 +25,26 @@ def filter_function(filter_column, filter_select, filter_csv, filter_file):
 
 
 def shorten_function(shorten_file, shorten_save):
-    new_file = shorten_file[['Id', 'Tags']].copy()
+    short_file = shorten_file[['Id', 'Tags']].copy()
     short_save = shorten_save + "_short.csv"
-    new_file.to_csv(short_save)
+    short_file.to_csv(short_save)
     print("File successfully generated as: " + short_save)
-    return new_file
+    return short_save, shorten_file
 
 
+def related_tag_function(related_shorten, related_save):
+    related_shorten['Tags'] = related_shorten['Tags'].str.replace(">", "")
+    split_date = related_shorten['Tags'].str.split("<")
+    data = split_date.to_list()
+    header = [1, 2, 3, 4, 5, 6]
+    tag_df = pd.DataFrame(data, columns=header)
+    new_df = tag_df[1].append(tag_df[2]).append(tag_df[3]).append(tag_df[4]).append(tag_df[5]).append(tag_df[6])
+    col_name = ['Tags']
+    total_tags_file = related_save + "_short.csv"
+    new_df.to_csv(total_tags_file, header=col_name)
+    CsvAction.delete_empty_rows(total_tags_file, "Tags")
+    return total_tags_file
+'''
 def related_function(related_shorten, related_file):
     related_shorten['Tags'] = related_shorten['Tags'].str.replace(">", "")
     split_data = related_shorten["Tags"].str.split("<")
@@ -48,6 +62,7 @@ def related_function(related_shorten, related_file):
     print("File successfully generated as: " + related_tags_file)
 
     return related_tags_file
+
 
 
 def number_function(related_save, related_tags):
@@ -78,6 +93,13 @@ def number_function(related_save, related_tags):
 
     print("File successfully generated as: " + related_output_csv)
 
+'''
+def count_total_function(file_path, new_path):
+    data = pd.read_csv(file_path)
+    header = ['Tags', 'Number_count']
+    data['Tags'].value_counts().reset_index().to_csv(new_path, header=header)
+
+
 
 def main():
     input_prompt = "Please enter the directory the .csv file is stored in. E.g. C:/Users/.../csv"
@@ -90,9 +112,13 @@ def main():
     else:
         sys.exit(1)
     csv_file = filter_function(selected_columns, selected_filter, filtered_csv, save_file)
-    shorten_csv = shorten_function(csv_file, save_file)
-    related_tags_file = related_function(shorten_csv, save_file)
-    number_function(save_file, related_tags_file)
+    shorten_save, shorten_csv = shorten_function(csv_file, save_file)
+    #related_tags_file = related_function(shorten_csv, save_file)
+    #number_function(save_file, related_tags_file)
+
+    related_tag_function(shorten_csv, save_file)
+    tag_combination = save_file + "_combination.csv"
+    count_total_function(shorten_save, tag_combination)
 
 
 if __name__ == "__main__":
