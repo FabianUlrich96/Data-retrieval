6b5f0121d74bcdfd9b718997a12c3c382f3e9905
Fabian Ulrich <fabian.ulrich@stud.uni-bamberg.de>
2020-06-10 12:06:02 +0200

    added shorten_function & count_values to CsvAction Class, changed filter_tags to related_tags, related_tags now creates a correct .csv file printing out the count of values

---
+++ related_tags.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import os.path
+import sys
+from UserSelection import UserSelection
+from CsvAction import CsvAction
+
+
+def input_filter():
+    input_term = input("Enter the filter term: ")
+    print(input_term)
+    return input_term
+
+
+def filter_function(filter_column, filter_select, filter_csv, filter_file):
+    filter_term = filter_csv[filter_column].str.contains(filter_select)
+    filtered_python = filter_csv[filter_term]
+    filter_file = filter_file + ".csv"
+    if os.path.exists(filter_file):
+        pass
+    else:
+        filtered_python.to_csv(filter_file)
+
+    return filtered_python
+
+
+def related_function(related_shorten, related_file):
+    related_shorten = pd.read_csv(related_shorten)
+    dropped_df = related_shorten.drop_duplicates(subset='Tags', keep="first", inplace=False)
+    dropped_df = dropped_df.drop(dropped_df.index[0:1])
+    related_tags_file = related_file + "_related_tags.csv"
+    dropped_df.to_csv(related_tags_file, header=True)
+
+    print("File successfully generated as: " + related_tags_file)
+
+    return related_tags_file
+
+
+def main():
+    input_prompt = "Please enter the directory the .csv file is stored in. E.g. C:/Users/.../csv"
+    selected_file = UserSelection.user_input(input_prompt)
+    if UserSelection.check_input(selected_file):
+        selected_filter = input_filter()
+        save_file = UserSelection.save_name()
+        selected_columns = "Tags"
+        filtered_csv = pd.read_csv(selected_file)
+    else:
+        sys.exit(1)
+    csv_file = filter_function(selected_columns, selected_filter, filtered_csv, save_file)
+    short_save = CsvAction.shorten_function(csv_file, save_file)
+    CsvAction.delete_empty_rows(short_save, "Tags")
+    related_function(short_save, save_file)
+    CsvAction.count_values(save_file + "_short.csv", "Tags", save_file + "_related_count.csv")
+
+
+if __name__ == "__main__":
+    main()
--- total_tag_count.py
+++ total_tag_count.py
@@ -1,20 +1,6 @@
 import pandas as pd
 from UserSelection import UserSelection
-from filter_tags import shorten_function
-
-
-def total_tags_function(count_shorten, count_name):
-    count_shorten['Tags'] = count_shorten['Tags'].str.replace(">", "")
-    split_date = count_shorten['Tags'].str.split("<")
-    data = split_date.to_list()
-    header = [1, 2, 3, 4, 5, 6]
-    tag_df = pd.DataFrame(data, columns=header)
-    new_df = tag_df[1].append(tag_df[2]).append(tag_df[3]).append(tag_df[4]).append(tag_df[5]).append(tag_df[6])
-    col_name = ['Tags']
-    total_tags_file = count_name + "_short.csv"
-    new_df.to_csv(total_tags_file, header=col_name)
-
-    return total_tags_file
+from CsvAction import CsvAction
 
 
 def delete_empty_rows(file_path):
@@ -40,11 +26,9 @@ def main():
     save_file = UserSelection.save_name()
     print(selected_file)
     selected_csv = pd.read_csv(selected_file)
-    short_csv = shorten_function(selected_csv, save_file)
-    total_tags_function(short_csv, save_file)
-    delete_empty_rows(save_file + "_short.csv")
-
-    count_total_function(save_file + "_short.csv", save_file + "_count.csv")
+    CsvAction.shorten_function(selected_csv, save_file)
+    CsvAction.delete_empty_rows(save_file + "_short.csv", "Tags")
+    CsvAction.count_values(save_file + "_short.csv", "Tags", save_file + "_count.csv")
 
 
 if __name__ == "__main__":
